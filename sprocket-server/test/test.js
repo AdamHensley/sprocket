d170e47b06531ae03246748cc93c331b72f6ee3c
Adam <ahensley@myms.io>
2017-10-11 22:24:22 -0700

    adding tests for all routes

--- sprocket-server/test/test.js
+++ sprocket-server/test/test.js
@@ -1,8 +1,5 @@
 var assert = require('assert');
-var routes = require('../routes/index');
 var request = require('supertest');
-var sinon = require('sinon');
-var proxyquire = require('proxyquire');
 var chai = require('chai');
 
 describe('GET /', function () {
@@ -22,7 +19,92 @@ describe('GET /currentPrice', function () {
             .get('/currentPrice')
             .expect(200, function (err, res) {
                 assert(res.body.currentPrice, /\d+/);
-                //assert.type(res.body.currentPrice,primitive.number);
+                done();
+            });
+    });
+});
+
+describe('PUT /buy with no body', function () {
+    it('should respond with an error message', function (done) {
+        request(require('../app.js'))
+            .put('/buy')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200, function (err, res) {
+                assert(res.body.error, "Missing Data from request!");
+                done();
+            });
+    });
+});
+
+describe('PUT /buy with good data', function () {
+    it('should respond with successful response', function (done) {
+        request(require('../app.js'))
+            .put('/buy')
+            .set('Accept', 'application/json')
+            .send({'sprockets':1, 'unit_price':25})
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200, function (err, res) {
+                assert(res.body.cash, 75);
+                assert(res.body.sprockets, 1);
+                assert(res.body.history.length, 1);
+                done();
+            });
+    });
+});
+
+describe('PUT /buy without enough money', function () {
+    it('should respond with an error message', function (done) {
+        request(require('../app.js'))
+            .put('/buy')
+            .set('Accept', 'application/json')
+            .send({'sprockets':5, 'unit_price':50})
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200, function (err, res) {
+                assert("You do not have enough money!!", res.body.error);
+                done();
+            });
+    });
+});
+
+describe('PUT /sell with no data', function () {
+    it('should respond with an error message', function (done) {
+        request(require('../app.js'))
+            .put('/sell')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200, function (err, res) {
+                assert(res.body.error, "Missing Data from request!");
+                done();
+            });
+    });
+});
+
+describe('PUT /sell with good data', function () {
+    it('should respond with successful response', function (done) {
+        request(require('../app.js'))
+            .put('/sell')
+            .set('Accept', 'application/json')
+            .send({'sprockets':1, 'unit_price':25})
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200, function (err, res) {
+                assert(res.body.cash, 100);
+                assert.deepStrictEqual(0, res.body.sprockets);
+                assert(2, res.body.history.length);
+                done();
+            });
+    });
+});
+
+describe('PUT /sell too many sprockets', function () {
+    it('should respond with an error message', function (done) {
+        request(require('../app.js'))
+            .put('/sell')
+            .set('Accept', 'application/json')
+            .send({'sprockets':5, 'unit_price':25})
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200, function (err, res) {
+                assert(res.body.error, "You do not have enough sprockets!!");
                 done();
             });
     });
